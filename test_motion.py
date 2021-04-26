2ef4079774560da7935dd55174486143ddbefd57
Dave Barnett <dave.barnett@jollygood.org.uk>
2021-04-26 21:10:44 +0100

    #1 findContours return changed for current OpenCV/Python version

--- test_motion.py
+++ test_motion.py
@@ -1,10 +1,10 @@
 from turret import VideoUtils
-import datetime
+from datetime import datetime
 
 def on_motion(contour, frame):
-    print(datetime.now, "MOTION")
+    print(datetime.now(), "MOTION")
     
 def on_no_motion(frame):
-    print(datetime.now, "NO MOTION")
+    print(datetime.now(), "NO MOTION")
     
 VideoUtils.find_motion(on_motion, on_no_motion, show_video=True)
--- turret.py
+++ turret.py
@@ -34,84 +34,86 @@ class VideoUtils(object):
         camera = cv2.VideoCapture(camera_port)
         time.sleep(0.25)
 
-        # initialize the first frame in the video stream
-        firstFrame = None
-        tempFrame = None
-        count = 0
+        try:
+            # initialize the first frame in the video stream
+            firstFrame = None
+            tempFrame = None
+            count = 0
 
-        # loop over the frames of the video
-        while True:
-            # grab the current frame and initialize the occupied/unoccupied
-            # text
+            # loop over the frames of the video
+            while True:
+                # grab the current frame and initialize the occupied/unoccupied
+                # text
 
-            (grabbed, frame) = camera.read()
+                (grabbed, frame) = camera.read()
 
-            # if the frame could not be grabbed, then we have reached the end
-            # of the video
-            if not grabbed:
-                break
-
-            # resize the frame, convert it to grayscale, and blur it
-            frame = imutils.resize(frame, width=500)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-            # if the first frame is None, initialize it
-            if firstFrame is None:
-                print ("Waiting for video to adjust...")
-                if tempFrame is None:
-                    tempFrame = gray
-                    continue
-                else:
-                    delta = cv2.absdiff(tempFrame, gray)
-                    tempFrame = gray
-                    tst = cv2.threshold(delta, 5, 255, cv2.THRESH_BINARY)[1]
-                    tst = cv2.dilate(tst, None, iterations=2)
-                    if count > 30:
-                        print ("Done.\n Waiting for motion.")
-                        if not cv2.countNonZero(tst) > 0:
-                            firstFrame = gray
-                        else:
-                            continue
-                    else:
-                        count += 1
-                        continue
-
-            # compute the absolute difference between the current frame and
-            # first frame
-            frameDelta = cv2.absdiff(firstFrame, gray)
-            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-
-            # dilate the thresholded image to fill in holes, then find contours
-            # on thresholded image
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            c = VideoUtils.get_best_contour(thresh.copy(), 5000)
-
-            if c is None:
-                callback_nomotion(frame)
-            else:
-                # compute the bounding box for the contour, draw it on the frame,
-                # and update the text
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                callback_motion(c, frame)
-
-            # show the frame and record if the user presses a key
-            if show_video:
-                cv2.imshow("Security Feed", frame)
-                key = cv2.waitKey(1) & 0xFF
-
-                # if the `q` key is pressed, break from the lop
-                if key == ord("q"):
+                # if the frame could not be grabbed, then we have reached the end
+                # of the video
+                if not grabbed:
                     break
 
-        # cleanup the camera and close any open windows
-        camera.release()
-        cv2.destroyAllWindows()
+                # resize the frame, convert it to grayscale, and blur it
+                frame = imutils.resize(frame, width=500)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+                # if the first frame is None, initialize it
+                if firstFrame is None:
+                    print ("Waiting for video to adjust...")
+                    if tempFrame is None:
+                        tempFrame = gray
+                        continue
+                    else:
+                        delta = cv2.absdiff(tempFrame, gray)
+                        tempFrame = gray
+                        tst = cv2.threshold(delta, 5, 255, cv2.THRESH_BINARY)[1]
+                        tst = cv2.dilate(tst, None, iterations=2)
+                        if count > 30:
+                            print ("Done.\n Waiting for motion.")
+                            if not cv2.countNonZero(tst) > 0:
+                                firstFrame = gray
+                            else:
+                                continue
+                        else:
+                            count += 1
+                            continue
+
+                # compute the absolute difference between the current frame and
+                # first frame
+                frameDelta = cv2.absdiff(firstFrame, gray)
+                thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+                # dilate the thresholded image to fill in holes, then find contours
+                # on thresholded image
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                c = VideoUtils.get_best_contour(thresh.copy(), 5000)
+
+                if c is None:
+                    callback_nomotion(frame)
+                else:
+                    # compute the bounding box for the contour, draw it on the frame,
+                    # and update the text
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    callback_motion(c, frame)
+
+                # show the frame and record if the user presses a key
+                if show_video:
+                    cv2.imshow("Security Feed", frame)
+                    key = cv2.waitKey(1) & 0xFF
+
+                    # if the `q` key is pressed, break from the lop
+                    if key == ord("q"):
+                        break
+
+        finally:
+            # cleanup the camera and close any open windows
+            camera.release()
+            cv2.destroyAllWindows()
 
     @staticmethod
     def get_best_contour(imgmask, threshold):
-        im, contours, hierarchy = cv2.findContours(imgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(imgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         best_area = threshold
         best_cnt = None
         for cnt in contours:
